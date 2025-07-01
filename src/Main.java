f9cdba752d175071239ea611960531604bfb3a8d
Mbhoni Shipalana <shipalanambhoni@gmail.com>
2025-07-01 22:45:40 +0200

    ome implementation now are working fine

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         // Load the FXML file
-        Parent root = FXMLLoader.load(getClass().getResource("/view/add_bursary.fxml")); // Make sure this path is correct
+        Parent root = FXMLLoader.load(getClass().getResource("/view/add_unive.fxml")); // Make sure this path is correct
         primaryStage.setTitle("Add Bursary");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
